43db955fc85efb53bc4aafe152f05b19825e1ed1
kdpsuite <kdpcreatorsuite@gmail.com>
2025-10-11 18:37:24 -0700

    Create app_theme.dart
    Added theme folder the core lib folder and created app_theme.dart

---
+++ kdp-creator-suite/lib/core/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    colorScheme: ColorScheme.light(
+      primary: Colors.blue, // Replace with your desired primary color
+      onPrimary: Colors.white,
+      secondary: Colors.green, // Replace with your desired secondary color
+      onSecondary: Colors.white,
+    ),
+  );
+}
