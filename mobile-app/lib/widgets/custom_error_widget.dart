b1bd54f009dd5a534d6e9362d2cdaa86dca64691
kdpsuite <kdpcreatorsuite@gmail.com>
2025-10-14 02:21:22 -0700

    Update custom_error_widget.dart
    Import app_theme.dart added

--- mobile-app/lib/widgets/custom_error_widget.dart
+++ mobile-app/lib/widgets/custom_error_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:kdp_creator_suite/theme/app_theme.dart';
 import '../core/app_export.dart';
 import 'package:kdp_creator_suite/theme/app_theme.dart';
 // custom_error_widget.dart
